b86af4180d5d9eee2c5a2bd0409c4f36f4a8487d
Matthew Rapati <mattrapati@gmail.com>
2018-05-05 15:14:24 -0400

    emitters

---
+++ Assets/Scripts/EnemyEmitter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEmitter : MonoBehaviour {
+
+    [SerializeField] GameObject enemy;
+    [SerializeField] public int remaining = 1;
+    [SerializeField] float rate = 1f;
+    [SerializeField] int scoreValue = 1;
+    [SerializeField] float delay = 0f;
+	[SerializeField] float range = 35f;
+
+	void Start () {
+		StartCoroutine(EmitEnemies());
+	}
+	
+    IEnumerator EmitEnemies() {
+        yield return new WaitForSeconds(delay);
+
+        while (remaining > 0) {
+			Vector3 randoSpot = new Vector3(Random.Range(-Mathf.Abs(range), range), 2f, Random.Range(-Mathf.Abs(range), range));
+            GameObject enemyInstance = Instantiate(enemy, randoSpot, Quaternion.identity);
+            Enemy enemyComponent = enemyInstance.GetComponent<Enemy>();
+            // enemyComponent.scoreValue = scoreValue;
+
+            remaining--;
+
+            yield return new WaitForSeconds(rate);
+        }
+    }
+}
--- Assets/Scripts/Potion.cs
+++ Assets/Scripts/Potion.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Potion : MonoBehaviour {
+    public int scoreValue = 1;
     bool isTouched = false;
 
     public void touch() {
