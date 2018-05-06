de2d22c24063e4de2b4942218a4569a5162b3481
Matthew Rapati <mattrapati@gmail.com>
2018-05-06 14:02:44 -0400

    title screen

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Menu : MonoBehaviour {
+
+	void Update () {
+		float submitRaw = CrossPlatformInputManager.GetAxisRaw("Submit");
+		float cancelRaw = CrossPlatformInputManager.GetAxisRaw("Cancel");
+
+		if (submitRaw == 1) {
+			SceneManager.LoadScene(1);
+		}
+
+		if (cancelRaw == 1) {
+            Application.Quit();
+		}
+	}
+}
