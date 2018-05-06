1e7666108f52cf1b3a74503a6f1ca852ad11b787
Matthew Rapati <mattrapati@gmail.com>
2018-05-06 16:51:18 -0400

    potion colliders updated, skull hit fx

--- Assets/Scripts/PlayerCollider2.cs
+++ Assets/Scripts/PlayerCollider2.cs
@@ -8,11 +8,13 @@ public class PlayerCollider2 : MonoBehaviour {
     GameManager gameManager;
 
     GameObject potionPickupFX;
+    GameObject skullCollideFX;
 
     void Start () {
         scoreBoard = FindObjectOfType<ScoreBoard>();
         gameManager = FindObjectOfType<GameManager>();
         potionPickupFX = Resources.Load("PotionPickup") as GameObject;
+        skullCollideFX = Resources.Load("SkullCollide") as GameObject;
     }
 
 	private void OnTriggerEnter(Collider other) {
@@ -22,7 +24,8 @@ public class PlayerCollider2 : MonoBehaviour {
             if (potion.hasHadABitOfATouch() == false) {
                 potion.touch();
                 
-                GameObject fx = Instantiate(potionPickupFX, transform.position, Quaternion.identity);
+                Vector3 thing = new Vector3(0, 5f, 0);
+                GameObject fx = Instantiate(potionPickupFX, transform.position + thing, Quaternion.identity);
                 fx.transform.parent = transform;
                 Destroy(fx, 1f);
                 
@@ -30,6 +33,11 @@ public class PlayerCollider2 : MonoBehaviour {
                 scoreBoard.SetPlayer2score(gameManager.updatePlayer2Score(1));
             }
         } else if (other.gameObject.tag == "Enemy") {
+            Vector3 thing = new Vector3(0, 5f, 0);
+            GameObject fx = Instantiate(skullCollideFX, transform.position + thing, Quaternion.identity);
+            fx.transform.parent = transform;
+            Destroy(fx, 1f);
+            
             Destroy(other.gameObject);
             scoreBoard.SetPlayer2score(gameManager.updatePlayer2Score(-1));
         }
